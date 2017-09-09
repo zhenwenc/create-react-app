c3762222e1b70bba843d774b031b508a9d1dec3c
Frederick Cai <frederick.cai@gmail.com>
2017-09-09 21:16:29 +1200

    [ts] Cleanup typedCssModulesLoader

--- packages/react-scripts/config/typedCssModulesLoader.js
+++ packages/react-scripts/config/typedCssModulesLoader.js
@@ -1,35 +1,22 @@
 'use strict';
 
-const path = require('path');
 const DtsCreator = require('typed-css-modules');
 const loaderUtils = require('loader-utils');
 
 module.exports = function(source, map) {
   this.cacheable && this.cacheable();
-  this.addDependency(this.resourcePath);
-  const callback = this.async();
+  const callback = this.async() || this.callback;
 
-  // Pass on query parameters as an options object to the DtsCreator. This lets
-  // you change the default options of the DtsCreator and e.g. use a different
-  // output folder.
   const queryOptions = loaderUtils.getOptions(this);
   const options = queryOptions ? Object.assign({}, queryOptions) : undefined;
   const creator = new DtsCreator(options);
 
-  // creator.create(..., source) tells the module to operate on the
-  // source variable. Check API for more details.
-  creator.create(this.resourcePath, source).then(content => {
-    // Emit the created content as well
-    this.emitFile(
-      path.relative(this.options.context, content.outputFilePath),
-      content.contents || [''],
-      map
-    );
-
-    // Output to file
-    console.log('[tcm] Wrote ' + content.outputFilePath);
-    content.writeFile().then(() => {
-      callback(null, source, map);
-    });
-  });
+  creator
+    .create(this.resourcePath, source)
+    .then(content => {
+      console.log('[tcm] Wrote ' + content.outputFilePath);
+      this.addDependency(content.outputFilePath);
+      return content.writeFile();
+    })
+    .then(() => callback(null, source, map), err => callback(err));
 };
